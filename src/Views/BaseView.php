bb032cfba5fc7ad25fe74f5b8b8769a528c9c801
BitsHost <dadryan26@yahoo.com>
2023-11-25 15:41:35 +0200

    upMVC-crud

--- src/Views/BaseView.php
+++ src/Views/BaseView.php
@@ -37,8 +37,8 @@ class BaseView
         echo '<head>';
         echo '<meta charset="UTF-8">';
         echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-        echo '<link rel="stylesheet" href="' . $appUrl . '/public/assets/css/styles.css">';
-        echo '<script src="' . $appUrl . '/public/assets/js/script.js"></script>';
+        echo '<link rel="stylesheet" href="' . $appUrl . '/vendor/bitshost/upmvc-crud/public/assets/css/styles.css">';
+        echo '<script src="' . $appUrl . '/vendor/bitshost/upmvc-crud/public/assets/js/script.js"></script>';
         echo '<title>PHP CRUD Example</title>';
         echo '</head>';
         echo '<body>';
